b3912d7db6562e4b202ec5dbcfabae29c933a7a6
samuca-canuto <canutosamuel9@gmail.com>
2025-10-20 10:09:39 -0300

    Update autoscaling-group.tf

--- autoscaling-group.tf
+++ autoscaling-group.tf
@@ -1,10 +1,7 @@
 resource "aws_autoscaling_group" "ecs_asg" {
   vpc_zone_identifier = [
     aws_subnet.sub-pub1.id,
-   // não sei se essa linha a seguir deve existir, pq as subs do tutorial sao duas duas
-   //pblicas, e a gente tem apenas uma publica 
-   //o auto scaling age apenas na priv
-   // aws_subnet.subnet2.id
+    aws_subnet.sub-pub2.id
   ]
   desired_capacity = 2
   max_size         = 2
