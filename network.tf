06a50571c7939db721b176efb8872fa94c733ca3
samuca-canuto <canutosamuel9@gmail.com>
2025-10-20 10:05:15 -0300

    Update network.tf

--- network.tf
+++ network.tf
@@ -8,7 +8,6 @@ resource "aws_vpc" "main" {
 
 resource "aws_subnet" "sub-pub1" {
   vpc_id = aws_vpc.main.id
-//cidr_block = 
   cidr_block = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone = "us-east-1a"
@@ -16,15 +15,13 @@ resource "aws_subnet" "sub-pub1" {
 
 resource "aws_subnet" "sub-priv1" {
   vpc_id = aws_vpc.main.id
-//cidr_block = 
-  cidr_block = "10.0.2.0/24"
+  cidr_block = "10.0.3.0/24"
   map_public_ip_on_launch = false
   availability_zone = "us-east-1a"
 }
 
-/*resource "aws_subnet" "sub-pub2" {
+resource "aws_subnet" "sub-pub2" {
   vpc_id = aws_vpc.main.id
-//cidr_block = 
   cidr_block = "10.0.2.0/24"
   map_public_ip_on_launch = true
   availability_zone = "us-east-1b"
@@ -32,12 +29,11 @@ resource "aws_subnet" "sub-priv1" {
 
 resource "aws_subnet" "sub-priv2" {
   vpc_id = aws_vpc.main.id
-//cidr_block = 
-  cidr_block = "10.0.2.0/24"
+  cidr_block = "10.0.4.0/24"
   map_public_ip_on_launch = false
   availability_zone = "us-east-1b"
 }
-*/
+
 
 resource "aws_internet_gateway" "igw" {  //decalra;'ao do igw
   vpc_id = aws_vpc.main.id
@@ -80,6 +76,18 @@ resource "aws_route_table_association" "pub_association" {
 //NEssa rota temos que apontar a sub_priv para o NAT (e para mais algum lugar?)
 resource "aws_route_table_association" "priv_association" {
   subnet_id      = aws_subnet.sub-priv1.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+// Associa a nova subnet pública à route table pública
+resource "aws_route_table_association" "pub_association_2" {
+  subnet_id      = aws_subnet.sub-pub2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+// Associa a nova subnet privada à route table privada
+resource "aws_route_table_association" "priv_association_2" {
+  subnet_id      = aws_subnet.sub-priv2.id
   route_table_id = aws_route_table.private_rt.id
 }
 
