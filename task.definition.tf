61f29bda579ddea758af9471c2549c2f06dc7f5d
samuca-canuto <canutosamuel9@gmail.com>
2025-10-21 09:05:57 -0300

    Update task.definition.tf

--- task.definition.tf
+++ task.definition.tf
@@ -1,23 +1,53 @@
+# Cria a role que o ECS vai usar para executar as tasks
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Anexa a política padrão de execução de tasks do ECS
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# Exemplo de task definition usando a role criada acima
 resource "aws_ecs_task_definition" "ecs_task_definition" {
   family             = "webapp"
   network_mode       = "awsvpc"
-  execution_role_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-  cpu                = 1024 # equivale a 1 vCPU - QUANTIDADE DE CPU A SER ALOCADA PARA A ECS 
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+  cpu                = 1024
+
   runtime_platform {
     operating_system_family = "LINUX"
     cpu_architecture        = "X86_64"
   }
-  
-  container_definitions = jsonencode([{
-    name      = "webapp-ctr"
-    image     = "strm/helloworld-http" # image URL of the application image
-    cpu       = 1024                    # equivale a 1 vCPU - QUANTIDADE DE CPU A SER ALOCADA PARA A ECS 
-    memory    = 256                     # 256 MiB = 0.25 GiB
-    essencial = true                    # a execução do container é essencial para a tarefa
-    portMappings = [{
-      containerPort = 80
-      hostPort      = 80
-      protocol      = "tcp"
-    }]
-  }])
+
+  container_definitions = jsonencode([
+    {
+      name      = "webapp-ctr"
+      image     = "strm/helloworld-http"
+      cpu       = 1024
+      memory    = 256
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+          protocol      = "tcp"
+        }
+      ]
+    }
+  ])
 }
