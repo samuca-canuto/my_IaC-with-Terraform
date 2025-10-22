9bf5338a541ab223f3a6cc30eada40fbc3955292
samuca-canuto <canutosamuel9@gmail.com>
2025-10-22 10:30:33 -0300

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -1,5 +1,5 @@
-resource "aws_alb" "ecs_alb" {
-  name = "ecs-alb"
+resource "aws_alb" "ec2_alb" {
+  name = "ec2-alb"
   internal = false
   load_balancer_type = "application"
   security_groups = [aws_security_group.security_group.id]
