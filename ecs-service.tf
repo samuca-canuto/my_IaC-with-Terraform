7568be769d893332eed839a666c6a1e316fb9e9a
samuca-canuto <canutosamuel9@gmail.com>
2025-10-27 08:06:00 -0300

    Update ecs-service.tf

--- ecs-service.tf
+++ ecs-service.tf
@@ -27,7 +27,7 @@ resource "aws_ecs_service" "ecs_service" {
   }
 
   load_balancer {
-    target_group_arn = aws_lb_target_group.ecs_tg.arn
+    target_group_arn = aws_lb_target_group.ec2_tg.arn
     container_name   = "webapp-ctr"
     container_port   = 80
   }
