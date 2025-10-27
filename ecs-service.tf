ed73513a2224cd2c5f9f3dbaea1a0e627e421c92
samuca-canuto <canutosamuel9@gmail.com>
2025-10-27 08:57:09 -0300

    Update ecs-service.tf

--- ecs-service.tf
+++ ecs-service.tf
@@ -2,30 +2,25 @@ resource "aws_ecs_service" "ecs_service" {
   name            = "webapp-svc"
   cluster         = aws_ecs_cluster.ecs_cluster.id
   task_definition = aws_ecs_task_definition.ecs_task_definition.arn
-  desired_count   = 2 # we want to run 2 instances of the container image on our ECS cluster
+  desired_count   = 2
+  launch_type     = "FARGATE"
 
   network_configuration {
-    security_groups    = [aws_security_group.security_group.id]
+    assign_public_ip = true
+    security_groups  = [aws_security_group.security_group.id]
     subnets = [
       aws_subnet.sub-pub1.id,
       aws_subnet.sub-pub2.id
     ]
   }
 
-  force_new_deployment = true
-
-  placement_constraints {
-    type = "distinctInstance"
-  }
-
-  triggers = {
-    redeployment = timestamp()
-  }
-
-
   load_balancer {
     target_group_arn = aws_lb_target_group.ec2_tg.arn
     container_name   = "webapp-ctr"
     container_port   = 80
   }
+
+  depends_on = [
+    aws_lb_listener.ec2_alb_listener
+  ]
 }
