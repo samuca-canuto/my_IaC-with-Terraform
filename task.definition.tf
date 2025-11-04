98a127fd1cc150742fc20b0683064594429beba7
samuca-canuto <canutosamuel9@gmail.com>
2025-11-04 09:53:31 -0300

    Update task.definition.tf - acrescentando signoz

--- task.definition.tf
+++ task.definition.tf
@@ -1,24 +1,42 @@
-# Create ECS Task Definition/Template
 resource "aws_ecs_task_definition" "ecs_task_definition" {
   family             = "webapp"
   network_mode       = "awsvpc"
-  execution_role_arn = "arn:aws:iam::787351301643:role/LabRole" //probelma aqui
-  cpu                = 1024 # equivale a 1 vCPU - QUANTIDADE DE CPU A SER ALOCADA PARA A ECS 
+  execution_role_arn = "arn:aws:iam::787351301643:role/LabRole" # Verifique se essa role tem permissões ECS Task Execution
+  cpu                = 1024
+
   runtime_platform {
     operating_system_family = "LINUX"
     cpu_architecture        = "X86_64"
   }
-  
-  container_definitions = jsonencode([{
-    name      = "webapp-ctr"
-    image     = "strm/helloworld-http" # image URL of the application image
-    cpu       = 1024                    # equivale a 1 vCPU - QUANTIDADE DE CPU A SER ALOCADA PARA A ECS 
-    memory    = 256                     # 256 MiB = 0.25 GiB
-    essencial = true                    # a execução do container é essencial para a tarefa
-    portMappings = [{
-      containerPort = 80
-      hostPort      = 80
-      protocol      = "tcp"
-    }]
-  }])
+
+  container_definitions = jsonencode([
+    {
+      name      = "webapp-ctr"
+      image     = "strm/helloworld-http"
+      cpu       = 1024
+      memory    = 256
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+          protocol      = "tcp"
+        }
+      ]
+      environment = [
+        {
+          name  = "OTEL_EXPORTER_OTLP_ENDPOINT"
+          value = "http://IP_DO_COLLECTOR:4318"
+        },
+        {
+          name  = "OTEL_SERVICE_NAME"
+          value = "webapp-ecs"
+        },
+        {
+          name  = "OTEL_EXPORTER_OTLP_PROTOCOL"
+          value = "http/protobuf"
+        }
+      ]
+    }
+  ])
 }
