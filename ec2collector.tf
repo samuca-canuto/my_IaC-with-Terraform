1d09c45bf78fda2fcb7667b9d0ff7a1ae1c84355
samuca-canuto <canutosamuel9@gmail.com>
2025-11-06 08:48:53 -0300

    Update ec2collector.tf

--- ec2collector.tf
+++ ec2collector.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "otel_collector" {
   instance_type          = "t3.micro"
   subnet_id              = aws_subnet.sub-pub1.id
   vpc_security_group_ids = [aws_security_group.security_group.id]
-  key_name               = "sua-chave-ssh" # substitua pela sua chave existente
+  key_name               = "vockey" # substitua pela sua chave existente
 
   associate_public_ip_address = true
 
