8ac6996e4601354185359a99c2362d9e3dca2cf0
samuca-canuto <canutosamuel9@gmail.com>
2025-11-10 10:28:31 -0300

    Update ec2collector.tf

--- ec2collector.tf
+++ ec2collector.tf
@@ -6,26 +6,20 @@ resource "aws_instance" "otel_collector" {
   key_name               = "vockey"
   associate_public_ip_address = true
 
-  user_data = <<EOF
-#!/bin/bash
-set -e
+user_data = <<-EOF
+  #!/bin/bash
+  set -e
 
-apt update -y
-apt install -y wget unzip
+  apt update -y
+  apt install -y wget unzip systemd
 
-# Instala o OpenTelemetry Collector
-wget https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.108.0/otelcol-contrib_0.108.0_linux_amd64.deb
-dpkg -i otelcol-contrib_0.108.0_linux_amd64.deb || true
+  cd /tmp
+  wget -q https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.131.1/otelcol-contrib_0.131.1_linux_amd64.deb
 
-# Garante que o binário existe
-if [ ! -f /usr/bin/otelcol-contrib ]; then
-  mv /usr/local/bin/otelcol-contrib /usr/bin/otelcol-contrib || true
-fi
+  dpkg -i otelcol-contrib_0.131.1_linux_amd64.deb || apt -f install -y
 
-# Cria diretório e arquivo de configuração
-mkdir -p /etc/otelcol-contrib
-
-cat <<EOT > /etc/otelcol-contrib/config.yaml
+  mkdir -p /etc/otelcol-contrib
+  cat <<EOT > /etc/otelcol-contrib/config.yaml
 extensions:
   health_check:
     endpoint: 0.0.0.0:13133
@@ -71,26 +65,11 @@ service:
       exporters: [otlp]
 EOT
 
-# Cria manualmente o serviço systemd
-cat <<EOT > /etc/systemd/system/otelcol-contrib.service
-[Unit]
-Description=OpenTelemetry Collector Contrib
-After=network.target
-
-[Service]
-ExecStart=/usr/bin/otelcol-contrib --config /etc/otelcol-contrib/config.yaml
-Restart=always
-User=root
-
-[Install]
-WantedBy=multi-user.target
-EOT
-
-# Recarrega o systemd e inicia o serviço
-systemctl daemon-reload
-systemctl enable otelcol-contrib
-systemctl start otelcol-contrib
+  systemctl daemon-reload
+  systemctl enable otelcol-contrib
+  systemctl restart otelcol-contrib
 EOF
+
 
   tags = {
     Name = "otel-collector"
