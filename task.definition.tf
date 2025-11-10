7d1f1f1d199da0bbbb7eeaeeb9d62fa355c02cca
samuca-canuto <canutosamuel9@gmail.com>
2025-11-10 09:49:14 -0300

    Update task.definition.tf

--- task.definition.tf
+++ task.definition.tf
@@ -1,3 +1,14 @@
+resource "aws_ecs_task_definition" "ecs_task_definition" {
+  family             = "webapp"
+  network_mode       = "awsvpc"
+  execution_role_arn = "arn:aws:iam::787351301643:role/LabRole"
+  cpu                = 1024
+
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "X86_64"
+  }
+
 container_definitions = jsonencode([
   {
     name      = "webapp-ctr"
