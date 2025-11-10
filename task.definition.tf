cd2f60ba5750e23fdc967b799c2a67ac28dce8c8
samuca-canuto <canutosamuel9@gmail.com>
2025-11-10 09:19:47 -0300

    Update task.definition.tf

--- task.definition.tf
+++ task.definition.tf
@@ -1,63 +1,46 @@
-resource "aws_ecs_task_definition" "ecs_task_definition" {
-  family             = "webapp"
-  network_mode       = "awsvpc"
-  execution_role_arn = "arn:aws:iam::787351301643:role/LabRole"
-  cpu                = 1024
-
-  runtime_platform {
-    operating_system_family = "LINUX"
-    cpu_architecture        = "X86_64"
-  }
-
-  container_definitions = jsonencode([
-    {
-      name      = "webapp-ctr"
-      image     = "strm/helloworld-http"
-      cpu       = 1024
-      memory    = 256
-      essential = true
-      portMappings = [
-        {
-          containerPort = 80
-          hostPort      = 80
-          protocol      = "tcp"
-        }
-      ]
-
-      environment = [
-        {
-          name  = "OTEL_SERVICE_NAME"
-          value = "webapp-ecs"
-        },
-        {
-          name  = "OTEL_EXPORTER_OTLP_ENDPOINT"
-          value = "https://ingest.us.signoz.cloud:443"
-        },
-        {
-          name  = "OTEL_EXPORTER_OTLP_PROTOCOL"
-          value = "http/protobuf"
-        },
-        {
-          name  = "OTEL_EXPORTER_OTLP_HEADERS"
-          value = "signoz-ingestion-key=b4e4cb2f-5337-4c86-a952-793acc1370a3"
-        },
-        {
-          name  = "OTEL_METRICS_EXPORTER"
-          value = "otlp"
-        },
-        {
-          name  = "OTEL_LOGS_EXPORTER"
-          value = "otlp"
-        },
-        {
-          name  = "OTEL_TRACES_EXPORTER"
-          value = "otlp"
-        },
-        {
-          name  = "OTEL_RESOURCE_ATTRIBUTES"
-          value = "service.name=webapp-ecs,deployment.environment=ecs,cloud.provider=aws"
-        }
-      ]
+container_definitions = jsonencode([
+  {
+    name      = "webapp-ctr"
+    image     = "strm/helloworld-http"
+    cpu       = 1024
+    memory    = 256
+    essential = true
+    portMappings = [
+      {
+        containerPort = 80
+        hostPort      = 80
+        protocol      = "tcp"
+      }
+    ]
+    logConfiguration = {
+      logDriver = "awslogs"
+      options = {
+        awslogs-group         = "/ecs/webapp"
+        awslogs-region        = "us-east-1"
+        awslogs-stream-prefix = "ecs"
+      }
     }
-  ])
-}
+    environment = [
+      {
+        name  = "OTEL_SERVICE_NAME"
+        value = "webapp-ecs"
+      },
+      {
+        name  = "OTEL_EXPORTER_OTLP_PROTOCOL"
+        value = "http/protobuf"
+      },
+      {
+        name  = "OTEL_EXPORTER_OTLP_ENDPOINT"
+        value = "https://ingest.us.signoz.cloud:443"
+      },
+      {
+        name  = "SIGNOZ_INGESTION_KEY"
+        value = "b4e4cb2f-5337-4c86-a952-793acc1370a3"
+      },
+      {
+        name  = "OTEL_EXPORTER_OTLP_HEADERS"
+        value = "signoz-ingestion-key=b4e4cb2f-5337-4c86-a952-793acc1370a3"
+      }
+    ]
+  }
+])
