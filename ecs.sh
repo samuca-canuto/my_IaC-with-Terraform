205afbd837fe6bcb1a5064f94e4d09cbfcb3aa35
samuca-canuto <canutosamuel9@gmail.com>
2025-11-14 10:44:45 -0300

    Update ecs.sh

--- ecs.sh
+++ ecs.sh
@@ -1,2 +1,46 @@
-#!bin/bash
-echo ECS_CLUSTER=ecs-cluster >> /etc/ecs/ecs.config;
+#!/bin/bash
+
+echo "ECS_CLUSTER=ecs-cluster" >> /etc/ecs/ecs.config
+
+# Atualiza pacotes
+sudo apt-get update -y
+
+# Instala o OpenTelemetry Collector
+wget https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.97.0/otelcol-contrib_0.97.0_linux_amd64.deb
+sudo dpkg -i otelcol-contrib_0.97.0_linux_amd64.deb
+
+# Cria o diretório de config
+mkdir -p /etc/otelcol-contrib/
+
+# Escreve o arquivo de configuração
+cat <<EOF >/etc/otelcol-contrib/config.yaml
+receivers:
+  otlp:
+    protocols:
+      grpc:
+      http:
+
+exporters:
+  otlp:
+    endpoint: "https://ingest.us.signoz.cloud:443"
+    headers:
+      signoz-ingestion-key: "b4e4cb2f-5337-4c86-a952-793acc1370a3"
+    tls:
+      insecure: false
+
+service:
+  pipelines:
+    metrics:
+      receivers: [otlp]
+      exporters: [otlp]
+    traces:
+      receivers: [otlp]
+      exporters: [otlp]
+    logs:
+      receivers: [otlp]
+      exporters: [otlp]
+EOF
+
+# Reinicia o collector com nova config
+sudo systemctl restart otelcol-contrib
+sudo systemctl enable otelcol-contrib
