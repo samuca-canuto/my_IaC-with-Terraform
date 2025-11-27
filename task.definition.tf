8f9be55c155cd4afc7cfc34ce7a5a1366a99b4f6
samuca-canuto <canutosamuel9@gmail.com>
2025-11-27 08:07:27 -0300

    Update task.definition.tf

--- task.definition.tf
+++ task.definition.tf
@@ -1,6 +1,6 @@
 resource "aws_ecs_task_definition" "ecs_task_definition" {
   family             = "webapp"
-  network_mode       = "bridge"
+  network_mode       = "awsvpc"
   requires_compatibilities = ["EC2"]
   execution_role_arn = "arn:aws:iam::787351301643:role/LabRole"
   cpu                = 1024
